78afb526b19f2889e538aebc36970b5cfdd19905
ferd-aboubakr <ferdaboubakr@gmail.com>
2026-09-01 15:51:36 +0100

    commit : exercice01

--- exercice-01.js
+++ exercice-01.js
@@ -1,1 +1,16 @@
+const prenom = "Amina";
+const ville = "Casablanca";
+let age = 22;
+let estEnFormation = true;
+let status = " Majeur.";
+if (age < 18)
+{
+    status = "Mineur ."
+}
+
+let res =  `${prenom} ${age} ans , habite a ${ville}. \n  Statut : ${status}\n  Formation en cours : ${estEnFormation}  \n\n\n  prenom -> ${typeof(prenom)} \n  age -> ${typeof(age)} \n  estEnFormation -> ${typeof(estEnFormation)} \n ` ;
+
+
+
+console.log(res);
 

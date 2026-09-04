81840f11b0abf9a7802c8223d4958a46ff28e3ad
ferd-aboubakr <ferdaboubakr@gmail.com>
2026-09-04 11:29:01 +0100

    fix: fixed syntax errors inside for loop & changed logic for string manipulation from a for loop to builtin javascript string functions like : (trim()/toLowerCase/..)

--- exercice-07.js
+++ exercice-07.js
@@ -2,28 +2,38 @@ const titre = " Mon Premier Projet MERN ";
 const phrase = "Le JavaScript est la base du stack MERN";
 const nomComplet = "amina el idrissi";
 
+// function genererSlug(titre) {
+
+
+// 	let newArr = "";
+
+// 	newArr = titre.split("");
+// 	let j = 0;
+
+// 	if(newArr[j] === " ")
+//    		j++;
+// 	for (let k= j ; k< titre.length ; k++)
+// 			{
+// 			 if(newArr[k] <= '95' && newArr[k] >= '65')
+// 			 { 
+//  				newArr[k] = newArr[k] - '32';
+// 			 } 
+// 			 else 
+// 			 {
+// 				newArr[k] = newArr[k] ;
+// 			 }
+
+
+// 			}
+// 			console.log(newArr);
+// 		}
 function genererSlug(titre) {
 
-	const i = titre.length;
+    const slug = titre.trim().toLowerCase().replaceAll(" ", "-");
+    console.log(slug);
 
-	let newArr = "";
-
-	newArr = titre.split("");
-	let j = 0;
-
-	if(newArr[j] === " ")
-   		j++;
-	for (let k= j , k< i, k++)
-			{
-			 if(newArr[k] <= 95 && newArr[k] >= 65)
-			 { 
- 				newArr[k] = newArr[k] + 32;
-			 } 
-
-
-	                 console.log(newArr);
-
-
-			}
+};
 
 genererSlug(titre);
+
+
